b4af23c156abac8917e3f60829865a8cc199db9d
YUJOO-LEE <lllllllllee@gmail.com>
2022-09-29 00:40:39 +0900

    220928 학습

--- 220928/vertical_layout/main.js
+++ 220928/vertical_layout/main.js
@@ -1,35 +1,55 @@
 window.addEventListener('load', ()=>{  // 페이지 로드 후 함수 실행
-  // vanilla JS
-  const grid = new Isotope('section', {
-    // options...
+
+  const grid = new Isotope('section', { // 인스턴스 생성
     itemSelector: 'article',
-    columnWidth: 'article',
-    transitionDuration: '0.5s',
+    columnWidth: 'article', // article의 width 값 가져옴
+    transitionDuration: '0.5s', // 모션 진행시간 (transition)
   });
 
-  const btns = document.querySelectorAll('main ul li');
+  const main = document.querySelector('main');
+  // document 한번만 읽어와서 속도 저하 방지
+  const articles = main.querySelectorAll('section article');
+  // article들을 유사배열로 가져옴
+  const btns = main.querySelectorAll('ul li');
+  // li들을 유사배열로 가져옴
 
   for (let el of btns) {
-    el.addEventListener('click', (e)=>{
-      e.preventDefault();
+    // 배열 길이만큼 반복하며 '값'을 하나씩 꺼내 사용
 
-      const sort = e.currentTarget.querySelector('a').getAttribute('href');
-      grid.arrange({
-        filter: sort,
+    el.addEventListener('click', function(e) {
+      // 선택 요소에 이벤트 부여
+
+      e.preventDefault();
+      // 클릭한 대상은 a태그 이므로 기본 기능 해제
+
+      const sort = e.target.getAttribute('href');
+      // 클릭한 a태그의 href 속성값 가져옴
+
+      grid.arrange({  // isotope 지정 메서드 
+        filter: sort, // 지정한 값을 가지는 요소만 filter
       });
 
-      for (let el of btns) {
-        el.classList.remove('on');
+      /*
+      for (let el of btns) {  // btns 돌면서
+        el.classList.remove('on');  // on class 찾아서 해제
       }
+      */
+      el.parentElement.querySelector('.on').classList.remove('on');
+      // on은 한개니까 이런식으로도 되지 않을까?
       e.currentTarget.classList.add('on');
+      // 현재 버튼에 on class 부여
+    })
+  }
+
+  for (let el of articles) {
+    el.querySelector('a').addEventListener('click', function(e) {
+      // favorite 버튼 클릭 이벤트 부여
+      e.preventDefault();
+
+      e.currentTarget.classList.toggle('on');
+      // 해당 버튼에 on 클래스 토글
+      el.classList.toggle('favorite');
+      // 해당 article에 favorite 클래스 토글
     })
   }
 })
-
-/*
-  BOM : 브라우저 모델(window, document, screen)
-  - window.onload 웹 브라우저의 모든 객체가 로드 되었을 때 실행
-
-  DOM : 문서 모델
-  - 
-*/
