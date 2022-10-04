e695d5143cde5f72d1beba5da9afc756cadd0ea0
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-04 17:59:31 +0900

    221004 학습

---
+++ 221004/review/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+  effect: "coverflow", 
+  grabCursor: true, 
+  centeredSlides: true, 
+  slidesPerView: "auto", 
+  coverflowEffect: {
+    rotate: 0, 
+    stretch: 0, 
+    depth: 100, 
+    modifier: 1, 
+    slideShadows: false,
+  },
+});
