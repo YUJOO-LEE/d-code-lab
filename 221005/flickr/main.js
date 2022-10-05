1097da86c8c58ea6068b1bd49f72f23045ec5887
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-05 12:30:29 +0900

    221005 학습

--- 221005/flickr/main.js
+++ 221005/flickr/main.js
@@ -7,17 +7,30 @@ const method2 = 'flickr.photos.search';
 const base = 'https://www.flickr.com/services/rest?';
 const perPage = 500;
 const format = 'json';
-const url1 = `${base}method=${method1}&api_key=${key}
+const urlAll = `${base}method=${method1}&api_key=${key}
 &per_page=${perPage}&format=${format}&nojsoncallback=1`;
-const url2 = `${base}method=${method2}&api_key=${key}
-&per_page=${perPage}&format=${format}&nojsoncallback=1&tags=바다&privacy_filter=1`;
 
-const list = document.querySelector('#list');
-const loading = document.querySelector('#loading');
+const $list = document.querySelector('#list');
+const $loading = document.querySelector('#loading');
+const $searchBox = document.querySelector('#searchBox form');
+const $input = $searchBox.querySelector('#search');
 
-callData(url1);
+callData(urlAll);
+
+$searchBox.addEventListener('submit', (e)=>{
+  e.preventDefault();
+
+  const tag = $input.value;
+  const urlSearch = `${base}method=${method2}&api_key=${key}&per_page=${perPage}&format=${format}&nojsoncallback=1&tags=${tag}&privacy_filter=1`;
+  callData(urlSearch);
+})
 
 function callData(url){
+  $list.innerHTML = '';
+  $loading.classList.remove('off');
+  $list.classList.remove('on');
+  $loading.querySelector('p').textContent = '0';
+
   fetch(url)
   .then(data=>{
     return data.json();
@@ -48,24 +61,23 @@ function createList(items){
     `;
   })
 
-  list.innerHTML = htmls;
+  $list.innerHTML = htmls;
 }
 
 function delayLoading(){
   // 이미지 로딩 확인
   // 동적으로 생성된 이미지 갯수 구함
-  const imgs = list.querySelectorAll('img');
+  const imgs = $list.querySelectorAll('img');
   const len = imgs.length;
   let count = 0;
-  loading.querySelector('p').textContent = count;
   
   for (let el of imgs) {
     el.addEventListener('load', ()=>{
       count++;
-      loading.querySelector('p').textContent = Math.floor(count/len * 100);
+      $loading.querySelector('p').textContent = Math.floor(count/len * 100);
 
       if(count === len) {
-        loading.querySelector('p').textContent = '100';
+        $loading.querySelector('p').textContent = '100';
         isoLayout();
       }
       // 로딩된 이미지 갯수가 리스트 총 갯수와 같아지면 함수실행
@@ -79,13 +91,13 @@ function delayLoading(){
 
 function isoLayout(){
 
-  loading.classList.add('off');
-  list.classList.add('on');
+  $loading.classList.add('off');
+  $list.classList.add('on');
   // 함수 호출 시 초기모션 추가
   // 모든 이미지가 로딩될 때 까지 isotope 적용되지 않으면 지저분해 보이기 때문에
   // 완성된 레이아웃 보여주기 위함
 
-  new Isotope( list, {
+  new Isotope( $list, {
     // options
     itemSelector: '.item',
     columnWidth: '.item',
