ea689083df8f7924f189a2d67e639921156d669c
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-06 22:13:24 +0900

    비밀번호 확인 조건별 분리 표시

--- 221006/form/validation.js
+++ 221006/form/validation.js
@@ -18,44 +18,46 @@ const $form = document.querySelector('#member');
 // SUBMIT 버튼
 $form.addEventListener('submit',(e)=>{
 
+  e.preventDefault();
   errorReset();
 
-  if (!checkLen('userId', 5)) {
+  if (checkLen('userId', 5)) {
     e.preventDefault();
     errorMsg('userId', '5글자 이상 입력하세요.');
   }
 
-  if (!checkPw('pw1', 6)) {
+  const pwError = checkPw('pw1', 6);
+  if (pwError) {
     e.preventDefault();
-    errorMsg('pw1', '비밀번호는 영문자, 숫자, 특수기호를 포함한 6자 이상으로 입력하세요.');
+    errorMsg('pw1', `비밀번호 양식에 맞게 입력하세요. (${pwError.join(', ')})`);
   }
 
-  if (!checkSamePw('pw1', 'pw2')) {
+  if (checkSamePw('pw1', 'pw2')) {
     e.preventDefault();
     errorMsg('pw2', '같은 비밀번호를 입력하세요.');
   }
 
-  if (!checkEmail('email')) {
+  if (checkEmail('email')) {
     e.preventDefault();
     errorMsg('email', '이메일 주소를 입력하세요.');
   }
 
-  if (!checkLen('edu')) {
+  if (checkLen('edu')) {
     e.preventDefault();
     errorMsg('edu', '최종 학력을 선택하세요.');
   }
 
-  if (!checkCheck('gender')) {
+  if (checkCheck('gender')) {
     e.preventDefault();
     errorMsg('gender', '성별을 선택하세요.');
   }
 
-  if (!checkCheck('hobby')) {
+  if (checkCheck('hobby')) {
     e.preventDefault();
     errorMsg('hobby', '취미를 선택하세요.');
   }
 
-  if (!checkLen('comments', 20)) {
+  if (checkLen('comments', 20)) {
     e.preventDefault();
     errorMsg('comments', '20글자 이상 입력하세요.');
   }
@@ -72,7 +74,7 @@ function checkLen(name, len = 1) {
   const $input = $form.querySelector(`[name=${name}]`);
   const txt = $input.value.trim();
 
-  return txt.length >= len ? true : false;
+  return txt.length < len ? true : false;
 }
 
 // 비밀번호 양식 확인
@@ -80,12 +82,19 @@ function checkPw(name, len = 6) {
   const $input = $form.querySelector(`[name=${name}]`);
   const txt = $input.value.trim();
 
-  const regEng = /[a-zA-Z]/;
-  const regNum = /[0-9]/;
-  const regSc = /[~!@#$%^&*()_+?<>₩]/;
-  const checkReg = regEng.test(txt) && regNum.test(txt) && regSc.test(txt) ? true : false;
+  const errReason = [];
+  if (txt.length < len) errReason.push(`${len}글자 이상`)
 
-  return txt.length >= len && checkReg ? true : false;
+  const regexp = {
+    '숫자 포함': /[0-9]/,
+    '영문자 포함': /[a-zA-Z]/,
+    '특수문자 포함': /[~!@#$%^&*()_+?<>₩]/
+  };
+  for (let exp of Object.keys(regexp)) {
+    if (!regexp[exp].test(txt)) errReason.push(exp);
+  }
+
+  return errReason.length ? errReason : false;
 }
 
 // 비밀번호 같은지 확인
@@ -93,7 +102,7 @@ function checkSamePw(pw1, pw2) {
   pw1 = $form.querySelector(`[name=${pw1}]`).value.trim();
   pw2 = $form.querySelector(`[name=${pw2}]`).value.trim();
 
-  return pw2 && pw1 === pw2 ? true : false;
+  return !pw2 || pw1 !== pw2 ? true : false;
 }
 
 // 이메일 조건 확인
@@ -102,7 +111,7 @@ function checkEmail(name){
   const txt = $input.value.trim();
   const regexp = /@/;
 
-  return regexp.test(txt) ? true : false;
+  return !regexp.test(txt) ? true : false;
 }
 
 // 체크 유무 확인
@@ -111,7 +120,7 @@ function checkCheck(name){
   let isChecked = false;
 
   for (let input of $inputs) if (input.checked) isChecked = true;
-  return isChecked ? true : false;
+  return !isChecked ? true : false;
 }
 
 // 셀렉트 선택 여부 확인 (checkLen()으로도 동작함)
@@ -120,7 +129,7 @@ function checkSelect(name){
   const selectId = $select.options.selectedIndex; // 선택된 옵션의 Index 반환
   const value = $select[selectId].value;
 
-  return value ? true : false;
+  return !value ? true : false;
 }
 
 // 에러 메세지 출력
