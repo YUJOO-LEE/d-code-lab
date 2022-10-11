1e79a242cfcc8beabe66260dad636a26c0b3f6f1
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-11 17:15:45 +0900

    221011 학습

--- 221011/react_basic/src/App.js
+++ 221011/react_basic/src/App.js
@@ -1,11 +1,23 @@
 import './css/style.css';
 import Footer from './components/Footer';
 import Header from './components/Layout';
+import image1 from './img/member1.jpg';
+import image2 from './img/member2.jpg';
 
 function App() {
+  const divStyle = {
+    width:300,
+    height:300,
+    background: `url(${image2}) no-repeat center/cover`
+  };
+
   return (
     <figure>
       <Header/>
+
+      <img src={image1} />
+      <div style={divStyle}></div>
+
       <Footer/>
     </figure>
   );
