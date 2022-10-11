ce3945e2339d6de2a807de55d531e80899047551
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-11 22:51:23 +0900

    221011 학습

--- 221011/react_basic/src/App.js
+++ 221011/react_basic/src/App.js
@@ -9,28 +9,33 @@ import Header from './components/Layout';
 
 2. 필요한 자리에서 process.env.PUBLIC_URL로 public의 절대 경로를 찾아 그 안에 있는 파일 사용
 
+class 사용 시 className으로 사용
+style 속성은 객체 형태로 사용
 */
 
 function App() {
-  const divStyle = {
-    width:300,
-    height:300,
-    background: `url(${process.env.PUBLIC_URL}img/member2.jpg) no-repeat center/cover`
-  };
-  /*
-  속성은 객체 형태로 사용
-  */
 
   return (
     <figure>
       <Header/>
-
-      <img src={process.env.PUBLIC_URL+'img/member1.jpg'} />
-      <div style={divStyle}></div>
-
+      <section>
+        <article>
+          <div className='inner'>
+            <div className='pic'>
+              <div className='dot'></div>
+            </div>
+            <div className='txt'>
+              <h2>Title</h2>
+              <p>Description</p>
+            </div>
+          </div>
+        </article>
+      </section>
       <Footer/>
     </figure>
   );
+
+  // return 내 주석 {/*  */}으로 가능하나 되도록 밖에 쓰기
 }
 
 // return은 하나의 요소만 가능하므로 fragment <></> 감싸서 내보내기
@@ -39,7 +44,7 @@ export default App;
 
 
 /*
-첫번째글자 대문자 필수
+내보내는 요소 첫번째글자 대문자 필수
 
 하나만 내보낼 경우 
 export default A;
--- 221011/react_basic/src/components/Layout.js
+++ 221011/react_basic/src/components/Layout.js
@@ -5,7 +5,7 @@ function Header() {
         <strong>DCODELAB</strong><br/>
         <span>UI/UX DESIGN & DEVELOPMENT</span>
       </h1>
-      <a href="#" className="menu">
+      <a href='#' className='menu'>
         <i className='fas fa-bars'></i>
       </a>
     </>
