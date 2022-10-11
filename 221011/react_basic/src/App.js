c50da69551e8fed469391a316f876538c7e87296
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-11 16:35:56 +0900

    221011 학습

--- 221011/react_basic/src/App.js
+++ 221011/react_basic/src/App.js
@@ -1,9 +1,12 @@
 import './css/style.css';
+import Footer from './components/Footer';
+import Header from './components/Layout';
 
 function App() {
   return (
     <figure>
-
+      <Header/>
+      <Footer/>
     </figure>
   );
 }
