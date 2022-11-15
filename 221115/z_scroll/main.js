3df87c0dcd3361c91cdb38fc4633a7fdf3fda6c2
YUJOO-LEE <lllllllllee@gmail.com>
2022-11-15 12:42:26 +0900

    스크롤 좌표에 대한 스타일 부여

---
+++ 221115/z_scroll/main.js
@@ -0,0 +1,20 @@
+const boxs = document.querySelectorAll('section article');
+const btns = document.querySelectorAll('ul li');
+const h1 = document.querySelector('h1');
+const distance = 3000;
+
+window.addEventListener('scroll', ()=>{
+  const scroll = window.scrollY;
+  h1.innerText = scroll;
+
+  boxs.forEach((box, idx)=>{
+    box.style.transform = `translateZ(${distance * idx * -1 + scroll}px)`;
+
+    if (scroll >= idx * distance) {
+      for (const btn of btns) {
+        btn.classList.remove('on');
+      }
+      btns[idx].classList.add('on');
+    }
+  })
+})
